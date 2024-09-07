9aae781356d828a7ebcd6d46991d8333cf5b52a4
Linus <element.oxygen@gmail.com>
2024-09-07 22:08:14 +0200

    adding more pipeline

--- sigma/pipelines/kusto/ms_xdr.py
+++ sigma/pipelines/kusto/ms_xdr.py
@@ -20,7 +20,7 @@ def ms_xdr() -> ProcessingPipeline:
         items=[
             # DeviceProcessEvents
             ProcessingItem(
-                identifier="set_state",
+                identifier="device_process_events",
                 transformation=SetStateTransformation(
                     key="query_table", val="DeviceProcessEvents"
                 ),
@@ -29,7 +29,7 @@ def ms_xdr() -> ProcessingPipeline:
                 ],
             ),
             ProcessingItem(
-                identifier="field_mapping",
+                identifier="field_mapping_device_process_events",
                 transformation=FieldMappingTransformation(
                     mapping={
                         "ProcessId": ["ProcessId"],
@@ -53,6 +53,147 @@ def ms_xdr() -> ProcessingPipeline:
                         "ParentUser": ["InitiatingProcessAccountName"],
                     }
                 ),
+                rule_conditions=[
+                    LogsourceCondition(category="process_creation"),
+                ],
+            ),
+            # DeviceImageLoadEvents
+            ProcessingItem(
+                identifier="device_image_load_events",
+                transformation=SetStateTransformation(
+                    key="query_table", val="DeviceImageLoadEvents"
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="image_load"),
+                ],
+            ),
+            ProcessingItem(
+                identifier="field_mapping_device_image_load_events",
+                transformation=FieldMappingTransformation(
+                    mapping={
+                        "ProcessId": ["InitiatingProcessId"],
+                        "Image": [
+                            "InitiatingProcessFolderPath # File path of the process that loaded the image"
+                        ],
+                        "ImageLoaded": ["FolderPath"],
+                        "FileVersion": ["InitiatingProcessVersionInfoProductVersion"],
+                        "Description": ["InitiatingProcessVersionInfoFileDescription"],
+                        "Product": ["InitiatingProcessVersionInfoProductName"],
+                        "Company": ["InitiatingProcessVersionInfoCompanyName"],
+                        "OriginalFileName": [
+                            "InitiatingProcessVersionInfoOriginalFileName"
+                        ],
+                        "sha1": ["SHA1"],
+                        "sha256": ["SHA256"],
+                        "md5": ["MD5"],
+                        "User": ["InitiatingProcessAccountName"],
+                        "Hashes": ["SHA1", "SHA256", "MD5"],
+                    }
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="image_load"),
+                ],
+            ),
+            # DeviceNetworkEvents
+            ProcessingItem(
+                identifier="device_network_events",
+                transformation=SetStateTransformation(
+                    key="query_table", val="DeviceNetworkEvents"
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="network_connection"),
+                ],
+            ),
+            ProcessingItem(
+                identifier="field_mapping_device_network_events",
+                transformation=FieldMappingTransformation(
+                    mapping={
+                        "ProcessId": ["InitiatingProcessId"],
+                        "Image": ["InitiatingProcessFolderPath"],
+                        "User": ["InitiatingProcessAccountName"],
+                        "Protocol": ["Protocol"],
+                        "SourceIp": ["LocalIP"],
+                        "SourceHostname": ["DeviceName"],
+                        "SourcePort": ["LocalPort"],
+                        "DestinationIp": ["RemoteIP"],
+                        "DestinationHostname": ["RemoteUrl"],
+                        "DestinationPort": ["RemotePort"],
+                    }
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="network_connection"),
+                ],
+            ),
+            # DeviceFileEvents
+            ProcessingItem(
+                identifier="device_file_events",
+                transformation=SetStateTransformation(
+                    key="query_table", val="DeviceFileEvents"
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="file_access"),
+                    LogsourceCondition(category="file_change"),
+                    LogsourceCondition(category="file_delete"),
+                    LogsourceCondition(category="file_event"),
+                    LogsourceCondition(category="file_rename"),
+                ],
+                rule_condition_linking=any,
+            ),
+            ProcessingItem(
+                identifier="field_mapping_device_file_events",
+                transformation=FieldMappingTransformation(
+                    mapping={
+                        "ProcessId": ["InitiatingProcessId"],
+                        "Image": ["InitiatingProcessFolderPath"],
+                        "TargetFilename": ["FolderPath"],
+                        "User": ["RequestAccountName"],
+                        "sha1": ["SHA1"],
+                        "sha256": ["SHA256"],
+                        "md5": ["MD5"],
+                    }
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="file_access"),
+                    LogsourceCondition(category="file_change"),
+                    LogsourceCondition(category="file_delete"),
+                    LogsourceCondition(category="file_event"),
+                    LogsourceCondition(category="file_rename"),
+                ],
+                rule_condition_linking=any,
+            ),
+            # DeviceRegistryEvents
+            ProcessingItem(
+                identifier="device_network_events",
+                transformation=SetStateTransformation(
+                    key="query_table", val="DeviceRegistryEvents"
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="registry_add"),
+                    LogsourceCondition(category="registry_delete"),
+                    LogsourceCondition(category="registry_event"),
+                    LogsourceCondition(category="registry_set"),
+                ],
+                rule_condition_linking=any,
+            ),
+            ProcessingItem(
+                identifier="field_mapping_device_network_events",
+                transformation=FieldMappingTransformation(
+                    mapping={
+                        "EventType": ["ActionType"],
+                        "ProcessId": ["InitiatingProcessId"],
+                        "Image": ["InitiatingProcessFolderPath"],
+                        "TargetObject": ["RegistryKey"],
+                        "Details": ["RegistryValueData"],
+                        "User": ["InitiatingProcessAccountName"],
+                    }
+                ),
+                rule_conditions=[
+                    LogsourceCondition(category="registry_add"),
+                    LogsourceCondition(category="registry_delete"),
+                    LogsourceCondition(category="registry_event"),
+                    LogsourceCondition(category="registry_set"),
+                ],
+                rule_condition_linking=any,
             ),
         ],
     )
